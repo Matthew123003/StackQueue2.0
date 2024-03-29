6e19ca1d7ccf860a5d009b08c90d4bfedc729736
Matt Kramer <matthew123003@gmail.com>
2024-03-29 08:58:10 -0400

    Tests done, implements done

--- src/main/java/com/matt/queuestack/Staque.java
+++ src/main/java/com/matt/queuestack/Staque.java
@@ -1,4 +1,75 @@
 package com.matt.queuestack;
 
-public class Staque {
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Stack;
+
+public class Staque<T> implements Collection<T> {
+
+    Stack<T> temp = new Stack<T>();
+    Stack<T> queue = new Stack<T>();
+    @Override
+    public int size() {
+        return queue.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
 }
